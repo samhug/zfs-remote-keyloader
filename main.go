0f2fa210d4c87706a29f9238422f87aaa919629f
Sam Hug <s@m-h.ug>
2018-12-22 02:40:18 +0000

    working unlock via echo -n 'testtest' | nc 127.0.0.1 3333

--- main.go
+++ main.go
@@ -3,13 +3,39 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"log"
+	"io/ioutil"
+	"net"
 	"os"
 	"os/exec"
 )
 
 func usage() {
 	fmt.Println("Usage: test-init-app")
+}
+
+func getIfaceList() ([]string, error) {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return nil, err
+	}
+	ips := []string{}
+	for _, iface := range ifaces {
+		addrs, err := iface.Addrs()
+		if err != nil {
+			return nil, err
+		}
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+			ips = append(ips, ip.String())
+		}
+	}
+	return ips, nil
 }
 
 func main() {
@@ -21,7 +47,62 @@ func main() {
 
 	fmt.Println("Hello from the golang test init app")
 
-	cmd := exec.Command("zfs", "load-key", "-L", "file:///test-key", "rpool/home2")
+	ips, err := getIfaceList()
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
+
+	l, err := net.Listen("tcp", "0.0.0.0:3333")
+	if err != nil {
+		fmt.Println("Error listening:", err.Error())
+		os.Exit(1)
+	}
+	defer l.Close()
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting: ", err.Error())
+			continue
+		}
+
+		go handleRequest(conn)
+	}
+}
+
+func handleRequest(conn net.Conn) {
+
+	defer conn.Close()
+
+	keyBuf := make([]byte, 512)
+
+	keyLen, err := conn.Read(keyBuf)
+	if err != nil {
+		fmt.Println("Error reading", err.Error())
+	}
+
+	conn.Write([]byte("Attempting to load zfs key:\n"))
+
+	f, err := ioutil.TempFile("/tmp", "zfs-key")
+	if err != nil {
+		fmt.Println("Error creating key file: ", err.Error())
+		return
+	}
+
+	defer os.Remove(f.Name())
+
+	key := keyBuf[:keyLen]
+
+	if _, err := f.Write(key); err != nil {
+		fmt.Println("Error writing to key file: ", err.Error())
+		return
+	}
+	if err := f.Close(); err != nil {
+		fmt.Println("Error closing key file: ", err.Error())
+		return
+	}
+
+	cmd := exec.Command("zfs", "load-key", "-L", fmt.Sprintf("file://%s", f.Name()), "rpool/home2")
 
 	var stdout bytes.Buffer
 	var stderr bytes.Buffer
@@ -29,9 +110,15 @@ func main() {
 	cmd.Stderr = &stderr
 
 	if err := cmd.Run(); err != nil {
-		fmt.Printf("Err.\n%q\n%q\n", stdout.String(), stderr.String())
-		log.Fatal(err)
+		conn.Write([]byte(fmt.Sprintf("Error loading zfs key with key='%q': \n", key)))
+		conn.Write(stdout.Bytes())
+		conn.Write(stderr.Bytes())
+		return
 	}
 
-	fmt.Printf("Done.\n%q\n%q\n", stdout.String(), stderr.String())
+	conn.Write(stdout.Bytes())
+	conn.Write(stderr.Bytes())
+
+	// we've successfully unlocked, shutdown the server
+	os.Exit(0)
 }
