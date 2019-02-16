cd39e9685544343e184c609ae775ce6ac2f8b219
Sam Hug <s@m-h.ug>
2019-02-16 18:49:34 +0000

    gracefully shutdown server on unlock

--- cmd/server.go
+++ cmd/server.go
@@ -1,15 +1,16 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/samhug/zfs-remote-keyloader/zfs"
 	"github.com/spf13/cobra"
 
 	"html/template"
+	"log"
 	"net"
 	"net/http"
-	"os"
 )
 
 var listenAddr string
@@ -31,7 +32,7 @@ var serverCmd = &cobra.Command{
 	Use:   "server",
 	Short: "Start HTTP remote key load server",
 	Long:  `Serves a web form over HTTP to prompt for ZFS dataset decryption keys`,
-	Run:   serverRun,
+	Run:   serverMain,
 }
 
 func getIfaceList() ([]string, error) {
@@ -60,15 +61,15 @@ func getIfaceList() ([]string, error) {
 }
 
 var server *http.Server
+var exitCh chan bool
 
-func serverRun(cmd *cobra.Command, args []string) {
+func serverMain(cmd *cobra.Command, args []string) {
 
-	fmt.Println("starting zfs-remote-keyloader server...")
+	log.Println("starting zfs-remote-keyloader server...")
 
 	ips, err := getIfaceList()
 	if err != nil {
-		fmt.Println("Failed to enumerate ip addresses: ", err.Error())
-		os.Exit(1)
+		log.Fatalln("Failed to enumerate ip addresses: ", err.Error())
 	}
 
 	fmt.Printf("Server has %d IP(s):\n", len(ips))
@@ -76,18 +77,28 @@ func serverRun(cmd *cobra.Command, args []string) {
 		fmt.Println(" -", ip)
 	}
 
-	fmt.Println("listening at ", listenAddr)
+	log.Println("listening at ", listenAddr)
 
 	http.HandleFunc("/", handleRequest)
 
 	server = &http.Server{
 		Addr: listenAddr,
 	}
+	exitCh = make(chan bool)
 
-	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		fmt.Println("HTTP Serve Error: ", err.Error())
-		os.Exit(1)
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatal(err)
+		}
+	}()
+
+	// Wait for the signal to shutdown the server
+	select {
+	case <-exitCh:
+		server.Shutdown(context.Background())
 	}
+
+	log.Println("Finished")
 }
 
 func handleRequest(w http.ResponseWriter, r *http.Request) {
@@ -118,9 +129,8 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 	}
 	responseTmpl.Execute(w, data)
 
-	// we've successfully unlocked, shutdown the server
-	//TODO: Should replace this with server.Shutdown /w a context
-	server.Close()
+	// We've successfully unlocked, send the signal to shutdown the server
+	exitCh <- true
 }
 
 type responseTmplData struct {
